5fde65b758e8a63c4ed6063243e27941b966904d
Agasthyanath_GS <49101776+Agasthyanath-GS@users.noreply.github.com>
2020-12-06 21:27:54 +0530

    Update export.py

--- export.py
+++ export.py
@@ -22,12 +22,12 @@ Swagger(app)
 
 #Creating a route with home page to check wheather the address and port of local host working correctly or not
 
-#@app.route('/')
-#def welcome():   
-#    return "WELCOME USER---type /apidocs for the swagger page where you can enter the data"
+@app.route('/')
+def welcome():   
+    return "WELCOME USER---type /apidocs for the swagger page where you can enter the data"
 
 # Creating a get request for collecting parametrs one by one for checking the "CHURN_FLAG" of a single 
-
+'''
 #creating Query set for running the swagger inide the flasgger
 @app.route('/predict',methods = ["Get"])
 def predict():    
@@ -123,6 +123,6 @@ def predict_file():
     return "predicteded 'CHURN_FLAG' for the input file is : " + str(prediction2.astype(int))
 
 #-> reading the data set,sciling the dataset,predicting the "CHURN_FLAG" ,printing the CHURN_FLAG in flasgger,saving the dataset with CHURN_FLAG as "test_data_out"
-
+'''
 if __name__ == "__main__":
     app.run()
